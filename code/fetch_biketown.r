8f1dc80025c43fe453a6f2d24ae3de7ab7755323
dpw-ordeq <wu.d@deq.state.or.us>
2019-04-08 13:40:16 -0700

    successful download

--- code/fetch_biketown.r
+++ code/fetch_biketown.r
@@ -1,6 +1,6 @@
 # functions to fetch public biketown trip data
 # see: https:// www.biketownpdx.com/system-data
-# subfolder: https://s3.amazonaws.com/biketown-tripdata-public/index.html
+# subfolder: https://s3.amazonaws.com/biketown-tripdata-public/
 
 # pacman allows checking for and installing missing packages 
 # by checking if pacman is installed yet and if not, installing it
@@ -21,6 +21,7 @@ install.packages("pkgs")
 get_data <- function(start, end,
                      base_url="https://s3.amazonaws.com/biketown-tripdata-public/",
                      outdir="data/biketown/") {
+  
   # make url function only available within get_data bc of base_url reference
   make_url <- function(date, base_url) {
     url <-paste0(base_url, format(date, "%Y_%m"), ".csv")
@@ -32,7 +33,7 @@ get_data <- function(start, end,
   end_date <- myd(end, truncated = 2)
   date_range <- seq(start_date, end_date, by="months")
   
-  # lapply(a,b) applies function bo to sequence a
+  # lapply(a,b) applies function b to sequence a
   # and returns a list of the modified sequence
   urls <- lapply(date_range, make_url, base_url = base_url)
   
