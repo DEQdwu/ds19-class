a25568688f0ae6cec5a767ae7d1b539a766e8d37
dpw-ordeq <wu.d@deq.state.or.us>
2019-04-08 10:58:23 -0700

    functions 2

--- code/Part_2_Data_Wrangling.r
+++ code/Part_2_Data_Wrangling.r
@@ -4,21 +4,32 @@
 # Fahrenheit to Kelvin
 far_to_kel <- function(temp) { 
   kel <- ((temp-32)*(5/9)) + 273.15  
-  return(kel)                  
 }
-far_to_kelvin(0)
+far_to_kel(0)
 
 # Convert Kelvin to Celsius
-kel_to_cel <- function(temp) {
+kel_to_cel <- function(temp) { 
   cel <- temp - 273.15
   return(cel)
 }
-kel_to_cel(0)
+kel_to_cel(273.15)
 
 # F to C using 2 functions just wrote
 far_to_cel <- function(temp) {
   far <- kel_to_cel(far_to_kel(temp))
-  return(Fah)
+  return(far)
 }
-Fah_to_C(212)
-Fah_to_C(32)
+far_to_cel(212)
+far_to_cel(32)
+
+# Convert Kelvin to Celsius; add if(is.numeric(temp)) function to check for number input
+# THIS DOES NOT WORK, RETURNS ERROR FUNCTION IN CASE OF NUMBER
+kel_to_cel2 <- function(temp) { 
+  if(is.numeric(temp)) {
+    stop("temp not numeric")
+    }
+  cel <- temp - 273.15
+  return(cel)
+}
+kel_to_cel2(2)
+kel_to_cel2(100)
---
+++ code/reference_another_function.r
@@ -0,0 +1,4 @@
+# referece a function in another file
+
+source("code/Part_2_Data_Wrangling.r")
+far_to_cel(212)
